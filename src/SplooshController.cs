cb1c1ca6818131c44470711ae0a1803964de629e
Lachlan Paulsen <32665702+NoNameWouldSuffice@users.noreply.github.com>
2025-05-12 16:34:03 +1200

    fix: hide the console cursor

--- src/SplooshController.cs
+++ src/SplooshController.cs
@@ -19,6 +19,8 @@ namespace Sploosh_Console
             boardView.homeRow = Console.CursorTop;
             boardView.homeCol = Console.CursorLeft;
 
+            Console.CursorVisible = false;
+
             while (!GameWon())
             {
                 boardView.Update();
